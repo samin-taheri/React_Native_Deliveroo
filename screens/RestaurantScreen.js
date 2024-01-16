65b59a09b9fe00905a744cc8001093468db6dd47
Samin Taheri <58706708+samin-taheri@users.noreply.github.com>
2024-01-16 17:31:52 +0300

    commiting changes

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -3,6 +3,7 @@ import React, { useLayoutEffect } from 'react'
 import { useNavigation, useRoute } from '@react-navigation/native'
 import { urlFor } from '../sanity';
 import { Feather } from "@expo/vector-icons";
+import DishRow from '../components/DishRow';
 
 const RestaurantScreen = () => {
     const navigation = useNavigation();
@@ -20,6 +21,42 @@ const RestaurantScreen = () => {
                 <TouchableOpacity onPress={navigation.goBack} className="absolute top-14 left-5 p-2 bg-gray-100 rounded-full">
                     <Feather name="arrow-left" size={30} color="#fe3448" />
                 </TouchableOpacity>
+            </View>
+            <View className="bg-white">
+                <View className="px-4 pt-4">
+                    <Text className="text-3xl font-bold">{title}</Text>
+                    <View className="flex-row space-x-2 my-1">
+                        <View className="flex-row items-center space-x-1">
+                            <Feather name="star" size={20} color="#fe3448" opacity={0.5} />
+                            <Text className="text-gray-500 text-xs">
+                                <Text className="text-gray-700">{rating}</Text> . {genre}
+                            </Text>
+                        </View>
+                        <View className="flex-row items-center space-x-1">
+                            <Feather name="map-pin" size={18} color="#fe3448" opacity={0.5} />
+                            <Text className="text-xs text-gray-500">Nearby . {address}</Text>
+                        </View>
+                    </View>
+                    <Text className="text-gray-500 mt-2 pb-4">{short_description}</Text>
+                </View>
+                <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
+                    <Feather name="help-circle" size={20} color="gray" opacity={0.5} />
+                    <Text className="font-bold  text-md pl-2 flex-1">Have a food alergy?</Text>
+                    <Feather name="chevron-right" size={20} color="gray" opacity={0.5} />
+                </TouchableOpacity>
+            </View>
+            <View>
+                <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
+                {dishes.map(dish => (
+                    <DishRow
+                        key={dish._id}
+                        id={dish._id}
+                        name={dish.name}
+                        description={dish.shortDescription}
+                        price={dish.price}
+                        image={dish.image}
+                    />
+                ))}
             </View>
         </ScrollView>
     )
